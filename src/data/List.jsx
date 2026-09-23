87743f12241aa3820b15a81141d923632053ff71
Luis Muñoz <143008760+DonLuisM@users.noreply.github.com>
2026-09-23 07:53:23 -0500

    Actualizar List.jsx

--- src/data/List.jsx
+++ src/data/List.jsx
@@ -72,6 +72,7 @@ export const messages = [
   "Entre más pasa el tiempo, más me sigues gustando, incluso después de 6 meses",
   "No sabes cuánto deseo que esto funcione y que sea por mucho tiempo",
   "Estoy orgulloso de todo lo que logras",
-  "Yo feliz de escucharte hablar tan profesionalmente, aunque no sepa mucho de medicina"
-  "
+  "Yo feliz de escucharte hablar tan profesionalmente, aunque no sepa mucho de medicina",
+  "Eres luz Jasbleidy Posu",
+  "Proverbios 3:15",
 ];
